a187ed49f5569a70e1c697881269c6ef1b2d924c
Charles Taylor <charlestaylor95@gmail.com>
2019-11-18 20:08:17 -0500

    No utility libraries

--- src/chapter1/1.1.js
+++ src/chapter1/1.1.js
@@ -1,5 +1,6 @@
+import "babel-polyfill"
 
-const cubeGraph = {
+export const cubeGraph = {
   vertices: Array.from({ length: 8 }, (_, i) => i),
   edges: [
     [0, 1],
@@ -17,4 +18,22 @@ const cubeGraph = {
   ]
 }
 
+export const getAdjacencyLists = graph => {
+  const { vertices, edges } = graph;
+  const lookup = Array.from(vertices, () => []);
+  for (let edge of edges) {
+    const [v1, v2] = edge;
+    lookup[v1].push(v2);
+    lookup[v2].push(v1);
+  }
+  return lookup;
+}
 
+// export const diff
+
+function* hamiltonianCircuits(adjLists, path) {
+  const n = adjLists.length;
+  if (path.length === n && path[0] === path[n]) {
+    yield path;
+  }
+}
--- src/chapter1/1.1.test.js
+++ src/chapter1/1.1.test.js
@@ -1,4 +1,14 @@
+import { cubeGraph, getAdjacencyLists } from './1.1'
 
-it('works', () => {
-  expect(true).toBeTruthy();
+test('getAdjacencyLists', () => {
+  const lists = getAdjacencyLists(cubeGraph);
+  expect(lists).toEqual([
+    [1, 2, 4],
+    [0, 3, 5],
+    [0, 3, 6],
+    [0, 5, 6],
+    [1, 4, 7],
+    [2, 4, 7],
+    [3, 6, 5],
+  ]);
 })
