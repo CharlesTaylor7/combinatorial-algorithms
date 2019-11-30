f3d5674cb7f51874d35741f5df7daffb6dbb666e
Charles Taylor <charlestaylor95@gmail.com>
2019-11-30 12:47:54 -0500

    wrap memoized values in a closure

--- src/chapter2/2.2.js
+++ src/chapter2/2.2.js
@@ -1,15 +1,18 @@
-const codes = [['']];
 
 export const binaryReflect = code => [
   ...code.map(x => `${0}${x}`),
   ...code.map(x => `${1}${x}`).reverse(),
 ];
 
-export const nthGrayCode = (n) => {
-  if (n > codes.length) {
-    for(let i = codes.length; i <= n; i++) {
-      codes.push[binaryReflect(codes[i-1])]
+export const nthGrayCode = (() => {
+  // memoize old gray codes
+  const codes = [['']];
+  (n) => {
+    if (n > codes.length) {
+      for(let i = codes.length; i <= n; i++) {
+        codes.push[binaryReflect(codes[i-1])]
+      }
     }
+    return codes[n];
   }
-  return codes[n];
-}
+})()
